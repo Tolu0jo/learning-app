58a7e2b0c11dc71a8294dde9afc2a1a82ebff1d1
toluOjo <ojotolulope137@gmail.com>
2024-08-28 08:42:36 +0100

    add role to login response object

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -36,6 +36,7 @@ export class AuthService {
     const payload = { email: user.email, role: user.role };
     return {
       access_token: this.jwtService.sign(payload),
+      role: user.role,
     };
   }
 }
